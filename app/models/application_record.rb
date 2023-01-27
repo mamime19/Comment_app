4352678b279dbd3c8d307e296ecb5f502efc0a3b
mamime19 <tatsu25sun@gmail.com>
2023-01-27 20:13:39 +0900

    succeed

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,8 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
-  before_action :configure_permitted_parameters, if: :devise_controller? 
-
-  private
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys:[name])
-  end
-endragit
+  primary_abstract_class
+end
