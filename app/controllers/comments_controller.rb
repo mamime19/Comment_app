18db80bb75c0b2a7de195dcbfc8cd5d9d9f28034
mamime19 <tatsu25sun@gmail.com>
2023-01-30 18:59:53 +0900

    cannot comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,10 +2,10 @@ class CommentsController < ApplicationController
 	def create
 		@comment=Comment.new(comment_params)
 		@comment.user_id=current_user.id
-		if comment.save
-			redirect_back(fall_back_location: root_path)
+		if @comment.save
+			redirect_back(fallback_location: root_path)
 		else
-			redirect_back(fall_back_location: root_path)
+			redirect_back(fallback_location: root_path)
 		end
 	end
 private
