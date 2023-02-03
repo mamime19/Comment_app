2efa4875f6932db6f12f20eb0473cc5ca81ed97e
mamime19 <tatsu25sun@gmail.com>
2023-02-03 21:38:04 +0900

    failed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
 	def create
-		@comment=Comment.new(comment_params)
+		@comment=current_user.comments.new(comment_params)
 		@comment.user_id=current_user.id
 		if @comment.save
 			redirect_back(fallback_location: root_path)
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,17 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:show, :create]
   def index
-    @posts=Post.all
-    @post=Post.new
+    @posts=current_user.posts.all
+    @post=current_user.posts.new
   end
 
   def show
     @post=Post.find(params[:id])
     @comments=@post.comments
-    @comment=Comment.new
+    @comment=current_user.comments.new
   end
   def create
-    @post=Post.new(post_params)
+    @post=current_user.posts.new(post_params)
     @post.user_id=current_user.id
     if @post.save
       redirect_back(fallback_location: root_path)
