0c75d27ee71031ac20af36f004568c65074d2994
mamime19 <tatsu25sun@gmail.com>
2023-02-15 18:55:16 +0900

    fixed little

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,7 @@ class PostsController < ApplicationController
   end
   def create
     @post=Post.new(post_params)
+    @post.user_id = current_user.id
     if @post.save
       redirect_back(fallback_location: root_path)
     else
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users, only: [:index, :show]
   resources :posts, only: [:index, :show, :create] do
      resources :comments, only: [:create]
   end
