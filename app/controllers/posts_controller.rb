67da65e3a3b78b0c1e0a791ea122f53a013a28ac
mamime19 <tatsu25sun@gmail.com>
2023-02-16 14:20:40 +0900

    design

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
 
   def show
     @post=Post.find(params[:id])
-    @comments=@comments = @post.comments.includes(:user).all
+    @comments= @post.comments.includes(:user).all
     @comment=@post.comments.build
   end
   def create
